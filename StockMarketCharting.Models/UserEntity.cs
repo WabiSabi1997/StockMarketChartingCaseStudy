0867dfaec99c2451f870ce7edff6fb41aa48b949
Snigdha Chaturvedi <SnigdhaChat@gmail.com>
2020-08-31 15:03:51 +0530

    Making changes in Models

--- StockMarketCharting.Models/UserEntity.cs
+++ StockMarketCharting.Models/UserEntity.cs
@@ -1,18 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace StockMarketCharting.Models
 {
     public class UserEntity
     {
-        public long ID { get; set; }
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30)]
         public string Username { get; set; }
+        [Required]
+        [StringLength(50,MinimumLength =6)]
         public string Password { get; set; }
-        public char UserType { get; set; }
+
+        [Required]
+        public UserType UserType { get; set; } //enum gets easy to use as a drop down list
         //Change to enum with User and Admin
+        [EmailAddress]
+        [Required]
         public string Email { get; set; }
-        public int Mobile { get; set; }
+        [Required]
+        public long Mobile { get; set; } //long makes the validation easier
+        
         public bool Confirmed { get; set; }
     }
 }
